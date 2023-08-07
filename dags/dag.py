8f0bc0044b898c552f3be51c157159e1d19da860
dmitry <dm_borisov@inbox.ru>
2023-08-07 18:10:24 +0300

    Add base ETL dag

---
+++ dags/dag.py
@@ -0,0 +1,40 @@
+from airflow import DAG
+from airflow.utils.dates import days_ago
+from airflow.operators.docker_operator import DockerOperator
+
+from datetime import timedelta
+from docker.types import Mount
+
+
+default_args = {
+    "owner": "airflow",
+    "description": "extract data from hh",
+    "depend_on_past": False,
+    "start_date": days_ago(1),
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5)
+}
+
+with DAG(
+    "docker_dag",
+    default_args=default_args,
+    schedule_interval="@daily") as dag:
+
+    extract_data = DockerOperator(
+        task_id="extract_data",
+        image="example",
+        api_version="auto",
+        auto_remove=True,
+        command="python extract.py",
+        docker_url="tcp://docker-proxy:2375",
+        mount_tmp_dir=False,
+        mounts = [
+            Mount(
+                source="hh-pipeline-data",
+                target="/app/data",
+                type="volume")
+        ],
+        network_mode="bridge"
+    )
+
+    extract_data

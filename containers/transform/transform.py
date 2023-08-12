1b04a40b1ebf42c9646c286f40e3fed74a5c5964
dmitry <dm_borisov@inbox.ru>
2023-08-12 17:35:35 +0300

    Remove decorators and add classes instead of them

--- containers/transform/transform.py
+++ containers/transform/transform.py
@@ -5,131 +5,200 @@ from flatten_json import flatten
 from sqlalchemy import insert
 from sqlalchemy.schema import Table
 from sqlalchemy.engine import Engine
-from models import engine, skills_table, vacancies_table
-from typing import Callable
-from functools import wraps
+from tables import engine, skills_table, vacancies_table
+from typing import Generator
 
 
-PATH = "data/extracted.jsonl"
+PATH = "extracted.jsonl"
 
 FORMAT = "[%(asctime)s] {%(filename)s} %(levelname)s %(message)s"
 logging.basicConfig(format=FORMAT, level=logging.INFO)
 
 
-class Transformer:
+class JsonlReaderMixin:
     """
-    A class that implements read and write functionality
-    to processing functions.
+    A mixin class that adds jsonl reading functionality.
+
+    Attributes
+    ------
+    path: str
+        A path to jsonl-file
+
+    Methods
+    -------
+    read_data()
+        A generator that yields one line pet time from jsonl-file
+    """
+    def __init__(self, path: str):
+        """
+        Attributes
+        ----------
+        path: str
+            A path to jsonl-file
+        """
+        self.__path = path
+
+    def read_data(self) -> Generator[dict, None, None]:
+        """A generator that yields one line per time from jsonl-file"""
+        with jsonlines.open(self.__path, "r") as reader:
+            yield from reader
+
+
+class DBWriterMixin:
+    """
+    A mixin class that adds writing to database functionality.
 
     Attributes
     ----------
-    path: str
-        path to the file with extracted data from head-hunter
     table: Table
-        table to write processed data
+        A database table to write processed data
+    engine: Engine
+        An instance of Engine class to create a connection with the database
+
+    Methods
+    -------
+    write_data()
+        Insert data to database from process data generator
+    """
+    def __init__(self, table: Table, engine: Engine):
+        self.__table = table
+        self.__engine = engine
+
+    def write_data(self):
+        """Insert data to database from process data generator"""
+        with self.__engine.connect() as conn:
+            for item in self.process_data():
+                conn.execute(insert(self.__table), item)
+                # conn.commit()
+
+        logging.info(f"writing data to {self.__table} is completed")
+
+
+class TableProcesser(JsonlReaderMixin, DBWriterMixin):
+    """
+    A class that processes extracted data as a table
+
+    Attributes
+    ----------
+    keys: list
+        A list of keys that represents attributes of database table
+    path: str
+        A path to jsonl-file
+    table: Table
+        A database table to write processed data
+    engine: Engine
+        An instance of Engine class to create a connection with the database
+
+    Methods
+    -------
+    process_data()
+        A generator that creates a dictionary with passed keys and populates it
+        with data from extracted object
+    """
+    def __init__(self, keys, path, table, engine):
+        """
+        Attributes
+        ----------
+        keys: list
+            A list of keys that represents attributes of a database table
+        path: str
+            Path to jsonl-file
+        table: Table
+            A database table to write processed data
+        engine: Engine
+            An instance of Engine class to create a connection with
+            the database
+        """
+        self.__keys = keys
+        super().__init__(path)
+        super(JsonlReaderMixin, self).__init__(table, engine)
+
+    def process_data(self):
+        """
+        A generator that creates a dictionary with passed keys and populates it
+        with data from extracted object
+
+        Yields
+        ------
+        A dictionary that represents database row
+        """
+        for obj in self.read_data():
+            # Flatten nested dictionary to easily retrieve fields
+            flatten_obj = flatten(obj)
+
+            items = dict.fromkeys(self.__keys)
+            for key in self.__keys:
+                if key in flatten_obj.keys():
+                    items[key] = flatten_obj[key]
+
+            yield items
+
+
+class AttributeProcesser(JsonlReaderMixin, DBWriterMixin):
+    """
+    A class that takes nested data and ids from extracted file,
+    and yields a dictionary with them as a database row
+
+    Attributes
+    ----------
+    key: str
+        A key for nested data
+    sub_key: str
+        A key for for a certain value of nested data item
+    attribute_name: str
+        A name of a database's column
+    path: str
+        Path to jsonl-file
+    table: Table
+        Database table to write processed data
     engine: Engine
         Instance of Engine class to create a connection with the database
 
     Methods
     -------
-    __call__(func)
-        Adds read and write functionality to processing function
+    process_data()
+        A generator that creates a pair of <id:attribute> and yields it
+        to the database
     """
-
-    def __init__(self, path: str, table: Table, engine: Engine):
+    def __init__(self, key, sub_key, attribute_name, path, table, engine):
         """
         Attributes
         ----------
+        key: str
+            A key for nested data
+        sub_key: str
+            A key for for a certain value of nested data item
+        attribute_name: str
+            A name of a database's column
         path: str
-            path to the file with extracted data from head-hunter
+            Path to jsonl-file
         table: Table
-            table to write processed data
+            Database table to write processed data
         engine: Engine
             Instance of Engine class to create a connection with the database
         """
-        self.__path = path
-        self.__table = table
-        self.__engine = engine
+        self.__key = key
+        self.__sub_key = sub_key
+        self.__attribute_name = attribute_name
+        super().__init__(path)
+        super(JsonlReaderMixin, self).__init__(table, engine)
 
-    def __call__(self, func: Callable):
+    def process_data(self):
         """
-        Adds read and write functionality to processing function
-
-        Parameters
-        ----------
-        func: Callable
-            processing function
+        A generator that creates a pair of <id:attribute> and yields it
+        to the database
         """
-        @wraps(func)
-        def wrapper(*args, **kwargs):
-            with (jsonlines.open(self.__path, "r") as reader,
-                  self.__engine.connect() as conn):
-                for obj in reader:
-                    data: dict = func(obj, *args, **kwargs)
-                    if data:
-                        conn.execute(insert(self.__table), data)
-                        conn.commit()
-
-                logging.info(f"writing data to {self.__table} is completed")
-
-        return wrapper
-
-
-@Transformer(PATH, vacancies_table, engine)
-def process_fields(obj: dict, keys: list) -> dict:
-    """
-    Retrieve necessary fields from object
-
-    Parameters
-    ----------
-    obj: dict
-        An item's dictionary retrieved from head-hunter api
-    keys: dict
-        A list of fields to retrieve
-
-    Returns
-    -------
-    A dictionary with necessary fields
-    """
-
-    # Flatten nested dictionary to easily retrieve fields
-    flatten_obj = flatten(obj)
-
-    items = dict.fromkeys(keys)
-    for key in keys:
-        if key in flatten_obj.keys():
-            items[key] = flatten_obj[key]
-
-    return items
-
-
-@Transformer(PATH, skills_table, engine)
-def process_nested_field(obj: dict, field: str, sub_field: str,
-                         name: str) -> list:
-    """
-    Create a list of dictionarties from dictionary-like field
-    from head-hunter file
-
-    Parameters
-    ----------
-    obj: dict
-        An item's dictionary retrieved from head-hunter api
-    field: str
-        A dictionary like field with necessary values
-    sub_field: str
-        A key that contain value
-    name: str
-        A name of table column
-
-    Return
-    ------
-    A list of dictionaries to insert into sql table
-    """
-
-    return [{"id": obj["id"], name: item[sub_field]} for item in obj[field]]
+        for obj in self.read_data():
+            if obj[self.__key]:
+                for item in obj[self.__key]:
+                    yield {"id": obj["id"],
+                           self.__attribute_name: item[self.__sub_key]}
 
 
 if __name__ == "__main__":
+    vacancies = TableProcesser(vacancy_keys2, PATH, vacancies_table, engine)
+    vacancies.write_data()
 
-    process_fields(vacancy_keys2)
-    process_nested_field("key_skills", "name", "skill")
+    skills = AttributeProcesser("key_skills", "name", "skill",
+                                PATH, skills_table, engine)
+    skills.write_data()

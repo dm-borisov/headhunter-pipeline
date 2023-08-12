bd670c91e996d18cdc5c694b1dd73e9b0985446e
dmitry <dm_borisov@inbox.ru>
2023-08-12 18:07:52 +0300

    Add new tables

--- containers/transform/tables.py
+++ containers/transform/tables.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy import MetaData
-from sqlalchemy import Table, Column, String, Integer
+from sqlalchemy import Table, Column, String, Integer, DateTime, Boolean
 
 
 metadata_obj = MetaData()
@@ -10,10 +10,17 @@ vacancies_table = Table(
     metadata_obj,
     Column("id", Integer),
     Column("name", String),
-    Column('alternate_url', String),
-    Column('salary_from', Integer),
-    Column('salary_to', Integer),
-    Column('published_at', String)
+    Column("area_id", Integer),
+    Column("employer_id", Integer),
+    Column("published_at", DateTime),
+    Column("experience_id", String),
+    Column("schedule_id", String),
+    Column("professional_roles", Integer),
+    Column("alternate_url", String),
+    Column("salary_from", Integer),
+    Column("salary_to", Integer),
+    Column("salary_currency", String),
+    Column("salary_gross", Boolean)
 )
 
 skills_table = Table(
@@ -21,6 +28,25 @@ skills_table = Table(
     metadata_obj,
     Column("id", Integer),
     Column("skill", String)
+)
+
+companies_table = Table(
+    "companies_tmp",
+    metadata_obj,
+    Column("id", Integer),
+    Column("accredited_it_employer", Boolean),
+    Column("name", String),
+    Column("type", String),
+    Column("area_id", Integer),
+    Column("site_url", Integer),
+    Column("alternate_url", Integer)
+)
+
+industries_talbe = Table(
+    "industries_tmp",
+    metadata_obj,
+    Column("id", Integer),
+    Column("industry", String)
 )
 
 path = "postgresql+psycopg2://postgres:postgres@172.17.0.1/postgres"

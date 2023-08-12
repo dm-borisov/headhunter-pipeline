aa17076ab3a81d072c7b66e4b294549dfeb3bda8
dmitry <dm_borisov@inbox.ru>
2023-08-12 18:15:02 +0300

    Add tables to initialize at the start

--- db-init/create_vac_tables.sql
+++ db-init/create_vac_tables.sql
@@ -1,13 +1,37 @@
 CREATE TABLE IF NOT EXISTS vacancies_load(
-    id             INT,
-    name           TEXT,
-    alternate_url  TEXT,
-    salary_from    INT,
-    salary_to      INT,
-    published_at   TIMESTAMP
+    id                 INT,
+    name               VARCHAR(50),
+    area_id            INT,
+    employer_id        INT,
+    published_at       TIMESTAMP,
+    experience_id      VARCHAR(20),
+    schedule_id        VARCHAR(20),
+    professional_roles INT,
+    alternate_url      VARCHAR(50),
+    salary_from        INT,
+    salary_to          INT,
+    salary_currency    VARCHAR(10),
+    salary_gross       BOOLEAN
 );
 
-CREATE TABLE IF NOT EXISTS skills_load(
+CREATE TABLE IF NOT EXISTS skills_tmp(
     id    INT,
-    skill TEXT
+    skill VARCHAR(50)
+);
+
+CREATE TABLE IF NOT EXISTS companies_tmp(
+    id                     INT,
+    accredited_it_employer BOOLEAN,
+    name                   VARCHAR(100),
+    type                   VARCHAR(20),
+    area_id                INT,
+    site_url               VARCHAR(50),
+    alternate_url          VARCHAR(50)
+);
+
+
+CREATE TABLE IF NOT EXISTS industries_tmp(
+    id        INT,
+    indsustry VARCHAR(100)
+
 );

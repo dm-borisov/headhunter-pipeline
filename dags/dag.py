a8f74f1335a89a4855dd5ac0e4181baa696b644c
dmitry <dm_borisov@inbox.ru>
2023-08-13 16:08:06 +0300

    Add tasks for dag

--- dags/dag.py
+++ dags/dag.py
@@ -20,15 +20,17 @@ with DAG(
     default_args=default_args,
     schedule_interval="@daily") as dag:
 
+    cmd = ('"Name:(data engineer OR data analyst)" '
+           '2023-08-08 2023-08-08 between3And6')
     extract_data = DockerOperator(
         task_id="extract_data",
         image="example",
         api_version="auto",
         auto_remove=True,
-        command="python extract.py",
+        command=cmd,
         docker_url="tcp://docker-proxy:2375",
         mount_tmp_dir=False,
-        mounts = [
+        mounts=[
             Mount(
                 source="hh-pipeline-data",
                 target="/app/data",
@@ -37,22 +39,43 @@ with DAG(
         network_mode="bridge"
     )
 
-    transform_data = DockerOperator(
-        task_id="transform_data",
+    columns = (
+        "id name area_id employer_id published_at "
+        "experience_id schedule_id professional_roles alternate_url "
+        "salary_from salary_to salary_currency salary_gross")
+    transform_vacancies = DockerOperator(
+        task_id="transform_vacancies",
         image="example2",
         api_version="auto",
         auto_remove=True,
-        command="python transform.py",
+        command=f"table vacancies_tmp -l {columns}",
         docker_url="tcp://docker-proxy:2375",
         mount_tmp_dir=False,
-        mounts = [
+        mounts=[
             Mount(
                 source="hh-pipeline-data",
                 target="/app/data",
                 type="volume")
         ],
-        network_mode="bridge"       
+        network_mode="bridge"
     )
 
+    keys = "-k key_skills -s name -a skill"
+    transform_skills = DockerOperator(
+        task_id="transform_skills",
+        image="example2",
+        api_version="auto",
+        auto_remove=True,
+        command=f"attribute skills_tmp {keys}",
+        docker_url="tcp://docker-proxy:2375",
+        mount_tmp_dir=False,
+        mounts=[
+            Mount(
+                source="hh-pipeline-data",
+                target="/app/data",
+                type="volume")
+        ],
+        network_mode="bridge"
+    )
 
-    extract_data >> transform_data
+    extract_data >> [transform_vacancies, transform_skills]

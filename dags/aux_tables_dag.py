603d82cfd0b3134cbd541255cab101ff99341698
dmitry <dm_borisov@inbox.ru>
2023-08-17 21:19:59 +0300

    Add dag for additional tables

---
+++ dags/aux_tables_dag.py
@@ -0,0 +1,31 @@
+from airflow import DAG
+from airflow.operators.docker_operator import DockerOperator
+from datetime import datetime, timedelta
+
+
+default_args = {
+    "owner": "airflow",
+    "description": "ETL process for headhunter api data (vacancies)",
+    "depend_on_past": False,
+    "start_date": datetime(2023, 8, 16),
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5)
+}
+
+with DAG(
+    "aux_tables_dag",
+    default_args=default_args,
+    schedule_interval="@once") as dag:
+
+    load_aux_tables = DockerOperator(
+        task_id="load_aux_tables",
+        image="example3",
+        api_version="auto",
+        auto_remove=True,
+        command="",
+        docker_url="tcp://docker-proxy:2375",
+        mount_tmp_dir=False,
+        network_mode="bridge"
+    )
+
+    load_aux_tables

b19ae4173ae243fda9863c97c99dcad8ae43a307
dmitry <dm_borisov@inbox.ru>
2023-08-18 13:28:01 +0300

    Return to the previous version. The problem was in dag not in the extracted field

--- containers/extract/hh_requests.py
+++ containers/extract/hh_requests.py
@@ -94,6 +94,4 @@ def get_data(url: str, params: dict, headers: dict,
 
     with jsonlines.open(path, mode='a') as writer:
         for url in get_urls(url, params, headers, key):
-            data = get_page(url, headers=headers)
-            del data["branded_description"]
-            writer.write(data)
+            writer.write(get_page(url, headers=headers))

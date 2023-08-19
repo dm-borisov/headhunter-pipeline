3c04b9aff46f9b6cc8e7177cdb59bdc8f9cff60d
dmitry <dm_borisov@inbox.ru>
2023-08-19 17:21:20 +0300

    Refactor sql insert scripts

--- dags/sql/insert_companies.sql
+++ dags/sql/insert_companies.sql
@@ -1,5 +1,11 @@
 INSERT INTO companies
-SELECT DISTINCT ct.*
+SELECT DISTINCT ct.id,
+       ct.accredited_it_employer,
+       ct.name,
+       ct.type,
+       ct.area_id,
+       ct.site_url,
+       ct.alternate_url
   FROM companies_{{ ds_nodash }} ct
        LEFT JOIN companies c ON ct.id = c.id
  WHERE c.id IS NULL;
--- dags/sql/insert_vacancies.sql
+++ dags/sql/insert_vacancies.sql
@@ -1,5 +1,17 @@
 INSERT INTO vacancies
-SELECT DISTINCT vt.*
+SELECT DISTINCT vt.id,
+       vt.name,
+       vt.area_id,
+       vt.employer_id,
+       vt.published_at,
+       vt.experience_id,
+       vt.schedule_id,
+       vt.professional_roles,
+       vt.alternate_url,
+       vt.salary_from,
+       vt.salary_to,
+       vt.salary_currency,
+       vt.salary_gross
   FROM vacancies_{{ ds_nodash }} vt
        LEFT JOIN vacancies v ON v.id = vt.id
  WHERE v.id IS NULL
